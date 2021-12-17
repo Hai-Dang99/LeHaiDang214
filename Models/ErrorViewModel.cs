1f84f7554c7707ed5351f54d891a77987e3aa5c2
Hai-Dang99 <haidang250899@gmail.com>
2021-12-17 14:56:32 +0700

    First_commit_LHD214

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeHaiDang214.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
